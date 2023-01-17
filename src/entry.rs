d1baf6d4d086fc457277fed259103f5d86531cbc
Alkajato <Alkajatomota@gmail.com>
2023-01-17 00:54:25 -0500

    Massive rework for less code overall.

--- src/entry.rs
+++ src/entry.rs
@@ -1,200 +1,108 @@
-use std::fs::{self, read};
+use std::{
+    fs::{self, read},
+    str,
+};
 
-#[derive(Clone)]
-pub struct Entry {
-    pub score: u32,
-    pub tags: Vec<u8>,
-    pub sources: String,
-    pub description: Vec<String>,
+pub fn scored_entries(args: &[String], in_dir: &str) -> Vec<String> {
+    let files_here = get_libraries(in_dir);
+    let mut entries: Vec<(usize, String)> = files_here
+        .iter()
+        .filter_map(|file| entries_from(file))
+        .flatten()
+        .filter_map(|entry| {
+            let tags = entry.lines().next().unwrap();
+            let score = score_entry(args, tags);
+            (score > 0).then_some((score, entry))
+        })
+        .collect();
+
+    // Sort entries by score.
+    entries.sort_by_key(|pair| pair.0);
+    entries.into_iter().map(|(_, entry)| entry).rev().collect()
 }
 
-impl Entry {
-    pub fn new() -> Entry {
-        let empty = String::from("");
-
-        Entry {
-            score: 0,
-            tags: vec![],
-            sources: empty.clone(),
-            description: vec![empty],
-        }
-    }
-}
-
-impl std::fmt::Display for Entry {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let output = write!(
-            f,
-            "{}\n{}",
-            String::from_utf8_lossy(&self.tags),
-            self.sources
-        );
-        for each in &self.description {
-            println!("{}", each);
-        }
-        output
-    }
-}
-
-pub fn get_entries(file: &str) -> Vec<Entry> {
-    let input = lines(file);
-    let mut output = Vec::new();
-
-    let mut i = 0;
-    while let Some(lines) = get_entry((i + 1) as i32, &input) {
-        output.push(Entry::new());
-
-        for line in lines {
-            let current_line: String = String::from_utf8_lossy(&line).to_string();
-
-            if is_tags(&line) {
-                output[i].tags = line;
-            } else if is_sources(&line) {
-                output[i].sources = current_line;
-            } else {
-                output[i].description.push(current_line);
-            }
-        }
-
-        i += 1;
-    }
-
-    output
-}
-
-/// Sorts input entries by count of how many tags the tag line contains.
-/// Higher scoring entries are near the beginning!
-pub fn sort_by_tags(input: Vec<Entry>, args: &[String]) -> Vec<Entry> {
-    let mut entries = input;
-    // Remove duplicate args so they can each only count once.
-    let mut de_dup_args = args.to_owned();
-    de_dup_args.sort();
-    de_dup_args.dedup();
-
-    for entry in &mut entries {
-        // For all entries
-        let mut str_chars = String::from_utf8_lossy(&entry.tags).to_lowercase();
-        str_chars.pop(); // Remove newline at the end of the string.
-        str_chars.pop(); // Remove ] at the end.
-        str_chars.remove(0); // Remove [ at the start.
-
-        // Remove duplicates from tags as well as input args.
-        let mut tags: Vec<&str> = str_chars.split_whitespace().collect();
-        tags.sort();
-        tags.dedup();
-
-        for arg in &de_dup_args {
-            // Compare all args against
-            for tag in &tags {
-                // each tag in current entry.
-                if *arg == *tag {
-                    entry.score += 1;
-                }
-            }
-        }
-    }
-
-    entries.sort_by_key(|item| item.score);
-    entries.reverse();
-
-    entries
-}
-
-/// Returns all .txt files at current working directory that contain entries.
-pub fn get_library_file_names() -> Vec<String> {
-    fs::read_dir("./")
-        .unwrap()
+/// Returns all entries from all files in current directory that were parsable as a library.
+fn get_libraries(in_dir: &str) -> Vec<String> {
+    fs::read_dir(in_dir)
+        .expect("Unable to open current directory!")
         .into_iter()
-        .map(|entry| entry.unwrap().path().to_str().unwrap().into())
-        .filter(|file_name: &String| is_library(file_name))
+        .filter_map(|entry| {
+            let Ok(entry) = entry else {
+                return None;
+            };
+            entry.path().to_str().map(String::from)
+        })
         .collect()
 }
 
-// Check if file contains even one tag line, with sources line afterwards.
-pub fn is_library(file: &str) -> bool {
-    if !file.contains(".txt") {
-        return false;
-    }
-
-    let input = lines(file);
-
-    for i in 1..input.len() {
-        if is_tags(&input[i - 1]) && is_sources(&input[i]) {
-            return true;
-        }
-    }
-
-    false
-}
-
-/// Returns a Vec of Vec<u8> with each Vec<u8> being each line from the file.
-fn lines(file: &str) -> Vec<Vec<u8>> {
-    let mut input = read(file).expect("File not found");
-    let (mut len, mut output, ln) = (input.len(), Vec::new(), b'\n');
-
-    // If file is empty return empty lines.
-    if len == 0 {
-        return vec![vec![]];
-    }
-
-    if input[len - 1] != ln {
-        input.push(ln);
-        len += 1;
-    }
-
-    let mut start = 0;
-    for i in start..len {
-        if input[i] == ln {
-            let addition = input[start..i].to_vec();
-            output.push(addition);
-
-            start = i + 1;
-        }
-    }
-
-    output
-}
-
-// Get entry from lines from guaranteed library file.
-// Freezes indefinitely if ran on non-library file.
-fn get_entry(target_entry: i32, lines: &Vec<Vec<u8>>) -> Option<Vec<Vec<u8>>> {
-    let mut output = Vec::new();
-    if target_entry <= 0 {
+/// Creates list of entries from file.
+/// Failure to read file, or then to interpret as utf8, returns `None`.
+fn entries_from(file: &str) -> Option<Vec<String>> {
+    let Ok(read_file) = read(file) else {
         return None;
-    }
+    };
 
-    let (mut entry_index, mut entry_count) = (0, 0);
-    while entry_count < target_entry {
-        if entry_index >= lines.len() {
-            return None;
-        }
+    let Ok(file_str) = str::from_utf8(&read_file) else {
+        return None;
+    };
 
-        if is_tags(&lines[entry_index]) {
-            entry_count += 1;
-        }
+    let lines: Vec<&str> = file_str.lines().collect();
+    let header_positions: Vec<usize> = lines
+        .iter()
+        .zip(lines.iter().skip(1))
+        .map(|(prev, current)| (prev.trim(), current.trim()))
+        .enumerate()
+        .filter_map(|(i, (prev, current))| (is_tags(prev) && is_sources(current)).then_some(i))
+        .collect();
 
-        entry_index += 1;
-    }
-
-    // The previous loop left entry_index ahead of the tags.
-    entry_index -= 1;
-    for i in entry_index..lines.len() {
-        output.push(lines[i].clone());
-
-        if i + 1 >= lines.len() || is_tags(&lines[i + 1]) {
-            break;
-        }
-    }
-
-    Some(output)
+    let mut ahead = header_positions.iter().skip(1);
+    Some(
+        header_positions
+            .iter()
+            .map(|index| {
+                if let Some(end) = ahead.next() {
+                    lines[*index..*end].join("\n")
+                } else {
+                    lines[*index..].join("\n")
+                }
+            })
+            .collect(),
+    )
 }
 
-// Check the beginning and ending character to tell if something is tags or sources.
-// We use len - 2 in both below because len - 1 is the newline at the end of the line.
-fn is_tags(str: &Vec<u8>) -> bool {
-    str[0] == b'[' && str[str.len() - 2] == b']'
+fn is_tags(input: &str) -> bool {
+    input.starts_with('[') && input.ends_with(']')
 }
 
-fn is_sources(str: &Vec<u8>) -> bool {
-    str[0] == b'{' && str[str.len() - 2] == b'}'
+fn is_sources(input: &str) -> bool {
+    input.starts_with('{') && input.ends_with('}')
+}
+
+/// Removes duplications in `args` and `tags`, then returns
+/// how many args in args matches any of the tags in tags.
+fn score_entry(args: &[String], tags: &str) -> usize {
+    let mut tags: Vec<String> = tags
+        .trim_start_matches('[')
+        .trim_end_matches(']')
+        .split_ascii_whitespace()
+        .map(|tag| tag.to_ascii_lowercase())
+        .collect();
+    tags.sort();
+    tags.dedup();
+
+    let mut args: Vec<String> = args.iter().map(|tag| tag.to_ascii_lowercase()).collect();
+    args.sort();
+    args.dedup();
+
+    args.iter()
+        .fold(0, |score, arg| score + (tags.contains(arg) as usize))
+}
+
+#[test]
+fn test_scoring() {
+    let tags = vec!["Bevy", "How", "To", "Spawn"];
+    let args: Vec<String> = tags.iter().map(|st| st.to_string()).collect();
+    for entry in scored_entries(&args, "D:\\Documents\\Study") {
+        println!("{entry}");
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,22 @@
+use entry::scored_entries;
 use std::env;
 mod entry;
 
 fn main() {
-    // First args is target\debug\library_reader.exe when cargo run the project.
+    // First args is target\debug\library_reader.&exe when cargo run the project.
     // Otherwise args is just what is inputted as expected.
     let args: Vec<String> = env::args().map(|item| item.to_lowercase()).collect();
 
-    // User provided no tags to search for
-    // For some reason the first arg is always the executable.
+    // // User provided no tags to search for
+    // // For some reason the first arg is always the executable.
     if args.len() == 1 {
         println!("Usage: sf Tag1 Tag2 Tag3....");
-        println!("Current folder should have file with tag line(s) \"[tag1 tag2 tag3]\" with source line next \"{{source1 source2 source3}}\"");
-
+        println!("Current folder should have file with tag line(s) \"[tag1 tag2 tag3]\" with sources line next \"{{source1 source2 source3}}\"");
         return;
     }
 
-    let libs = entry::get_library_file_names();
-    if libs.len() == 0 {
-        println!("No libraries found in current folder");
-        return;
-    }
-
-    // Gather all .txt files that are libraries into the entries Vec.
-    let entries: Vec<entry::Entry> = libs
-        .into_iter()
-        .flat_map(|name| entry::get_entries(&name))
-        .collect();
-
-    let sorted = entry::sort_by_tags(entries, &args);
-    if sorted.iter().all(|item| item.score == 0) {
-        print!("No matches for: ");
-        for arg in env::args().skip(1) {
-            print!("{arg}");
-        }
-
-        return;
-    }
-
-    for entry in sorted.iter().filter(|entry| entry.score > 0) {
+    // Print out those with score higher than zero.
+    for entry in scored_entries(&args, "./") {
         println!("{entry}");
     }
 }
